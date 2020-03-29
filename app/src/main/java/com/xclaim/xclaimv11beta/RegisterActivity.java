d2d14ec208f7ca3cfc58edd989b0b028bb846f67
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2020-03-29 14:45:38 +0800

    Firestore Database Setup

--- app/src/main/java/com/xclaim/xclaimv11beta/RegisterActivity.java
+++ app/src/main/java/com/xclaim/xclaimv11beta/RegisterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,18 +15,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
-   EditText mFullName,mEmail,mPassword,mPhone;
+    public static final String TAG = "TAG";
+    EditText mFullName,mEmail,mPassword,mPhone;
    Button mRegisterBtn;
    TextView mLoginBtn;
    FirebaseAuth fAuth;
    ProgressBar progressBar;
+
+   FirebaseFirestore fstore;
+   String userID;
 
 
 
@@ -43,6 +55,8 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
         progressBar = findViewById(R.id.progressBar);
 
 
@@ -62,8 +76,11 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+
+                final String fullname = mFullName.getText().toString().trim();
+                final String phone = mPhone.getText().toString().trim();
 
 
                 if(TextUtils.isEmpty(email)){
@@ -89,6 +106,31 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(RegisterActivity.this, "User Created." , Toast.LENGTH_SHORT).show();
+
+
+                            //User ID using firebase reference
+                            userID = fAuth.getCurrentUser().getUid();
+
+                            //Processing data to and from firestore
+                            DocumentReference documentReference = fstore.collection("users").document(userID); //will auto create the collection if not present
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fName", fullname);
+                            user.put("Email", email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() { //checks if uploading to database is succesful
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: userProfile is created for " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure:" + e.toString());
+                                }
+                            });
+
+
+
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                             
                         } else{
